196313540e643ca2fce61fcc185fda666bc53db5
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-20 10:28:54 -0800

    added a when start is equal to end test in frogJumps.spec.js

--- __tests__/frogJumps.spec.js
+++ __tests__/frogJumps.spec.js
@@ -1,1 +1,10 @@
 const frogJumps = require("../lib/frogJumps.js");
+
+describe("Frogs a Jumpin", () => {
+  describe("when start is equal to end", () => {
+    it("should return 0", () => {
+      const result = frogJumps(10, 10, 5);
+      expect(result).toEqual(0);
+    });
+  });
+});

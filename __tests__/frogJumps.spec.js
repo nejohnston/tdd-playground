b73c80cd3517cf4ae231ba6f65ff2197b69a78f6
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-20 11:51:44 -0800

    added when start is less than 0, should return 0

--- __tests__/frogJumps.spec.js
+++ __tests__/frogJumps.spec.js
@@ -1,10 +1,15 @@
-const frogJumps = require("../lib/frogJumps.js");
+const frogJumps = require("../lib/frogJumps");
 
-describe("Frogs a Jumpin", () => {
-  describe("when start is equal to end", () => {
-    it("should return 0", () => {
-      const result = frogJumps(10, 10, 5);
-      expect(result).toEqual(0);
-    });
+describe("frogJumps", () => {
+  test("when start is equal to end, should return 0", () => {
+    const result = frogJumps(10, 10, 5);
+    expect(result).toEqual(0);
+  });
+  test("when start is less 0, should return 0", () => {
+    const result = frogJumps(-1, 10, 5);
+    expect(result).toEqual(0);
+  });
+  test("when end is equal to 10, should return end", () => {
+    expect(frogJumps(5, 15, 10)).toEqual(1);
   });
 });

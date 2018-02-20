427210195c6d5735baffd4256a7c2ab1d5a5a687
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-20 13:23:58 -0800

    changed all instances of palindromes including file names, to palindrome

---
+++ __tests__/palindrome.spec.js
@@ -0,0 +1,5 @@
+const palindrome = require("../lib/palindrome");
+
+describe("palindrome", () => {
+  test("when palindrome is equal to palindrome, return true", () => {});
+});
--- __tests__/palindromes.spec.js
+++
@@ -1,5 +0,0 @@
-const palindromes = require("../lib/palindromes");
-
-describe("palindromes", () => {
-  test("when palindrome is equal to palindrome, return true", () => {});
-});

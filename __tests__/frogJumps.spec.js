16477a31c870573c72aa5b5a0d8fd73cb28bb32c
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-20 13:41:59 -0800

    added jest eslint plugin

--- __tests__/frogJumps.spec.js
+++ __tests__/frogJumps.spec.js
@@ -1,19 +1,19 @@
-const frogJumps = require("../lib/frogJumps");
+const frogJumps = require('../lib/frogJumps');
 
-describe("frogJumps", () => {
-  test("when start is equal to end, should return 0", () => {
+describe('frogJumps', () => {
+  test('when start is equal to end, should return 0', () => {
     const result = frogJumps(10, 10, 5);
     expect(result).toEqual(0);
   });
-  test("when start is less 0, should return 0", () => {
+  test('when start is less 0, should return 0', () => {
     const result = frogJumps(-1, 10, 5);
     expect(result).toEqual(0);
   });
-  test("when end is equal to 10, should return end", () => {
+  test('when end is equal to 10, should return end', () => {
     const result = frogJumps(0, 10, 5);
     expect(result).toEqual(10);
   });
-  test("when start, end, jumpLength equal undefined, return undefined");
+  test('when start, end, jumpLength equal undefined, return undefined');
   const result = frogJumps(undefined, undefined, undefined);
   expect(result).toEqual(undefined);
 });

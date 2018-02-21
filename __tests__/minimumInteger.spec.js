e40cdf45208e834b5c22060f605ca0f2b11ec7ec
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-21 13:41:09 -0800

    added first test to minimumInteger spec

--- __tests__/minimumInteger.spec.js
+++ __tests__/minimumInteger.spec.js
@@ -1,1 +1,8 @@
-const minimumInteger = require('minimumInteger.js');
+const minimumInteger = require('../lib/minimumInteger');
+
+describe('minimumInteger', () => {
+  test('', () => {
+    const result = minimumInteger([1, 3, 6, 4, 1, 2]);
+    expect(result).toEqual([5]);
+  });
+});

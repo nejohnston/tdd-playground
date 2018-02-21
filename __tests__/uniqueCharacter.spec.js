1d1247c07528f1ad82c3b35091b2ba965837fcca
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-21 09:37:38 -0800

    added first test to uniqueCharacter.spec.js

--- __tests__/uniqueCharacter.spec.js
+++ __tests__/uniqueCharacter.spec.js
@@ -1,1 +1,8 @@
 const uniqueCharacter = require('../lib/uniqueCharacter');
+
+describe('uniqueCharacter', () => {
+  test('when given a string with a unique character, return character', () => {
+    const result = uniqueCharacter('dddeefggghhh');
+    expect(result).toEqual('f');
+  });
+});

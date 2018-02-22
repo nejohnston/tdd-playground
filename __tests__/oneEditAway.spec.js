19bcebf3492782636aebb9ce31c96cb5b3855276
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-21 16:12:06 -0800

    added first test to oneEditAway.spec

--- __tests__/oneEditAway.spec.js
+++ __tests__/oneEditAway.spec.js
@@ -1,1 +1,8 @@
 const oneEditAway = require('../lib/oneEditAway');
+
+describe('oneEditAway', () => {
+  test('when given two strings that are equal minus one letter, return true', () => {
+    const result = oneEditAway('pale', 'ple');
+    expect(result).toBeTruthy();
+  });
+});
